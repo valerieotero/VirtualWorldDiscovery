57e2c71bf9c711b28e84a503bb80d66263762458
Yamil Gonzalez Zuaznabar <yamil.gonzalez4@upr.edu>
2020-03-05 22:35:24 -0400

    Added writer and reader classes

---
+++ src/Reader.java
@@ -0,0 +1,7 @@
+
+public class Reader {
+
+	public static void main(String[] args) {
+	}
+
+}
---
+++ src/Writer.java
@@ -0,0 +1,49 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Writer {
+	static BufferedWriter configFile;
+	
+	//main tester
+	//delete after use
+	public static void main(String[] args) {
+		String f = "configuration";
+		String w = "I can open the file";
+		open(f);
+		write(w);
+		write("im yamil");
+		close();
+	}
+
+	//Will try to open the file wanted
+	public static void open(String fileName) {
+		try {
+			configFile = new BufferedWriter(new FileWriter(fileName));
+		}catch (IOException e) {
+			System.out.println("An error occurred.");
+			e.printStackTrace();
+		}
+	}
+	
+	//Will try to close the file that is already open
+	public static void close() {
+		try {
+			configFile.close();
+		}catch (IOException e) {
+			System.out.println("An error occurred.");
+			e.printStackTrace();
+		}
+	}
+	
+	//Will try to write a sentence into the file
+	public static void write(String sentence) {
+		try {
+			configFile.write(sentence);
+			configFile.newLine();
+		}catch (IOException e) {
+			System.out.println("An error occurred.");
+			e.printStackTrace();
+		}
+	}
+}
