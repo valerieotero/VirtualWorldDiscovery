600f105c7a45284495310b47dd5694375d8a45fb
Valerie <valerie.otero2@upr.edu>
2020-04-23 10:33:26 -0400

    answers appear with first question

--- src/GamePanels/TakeTestPanel.java
+++ src/GamePanels/TakeTestPanel.java
@@ -5,6 +5,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.Map.Entry;
 
 import javax.swing.JButton;
@@ -13,124 +14,152 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import Classes.Reader;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class TakeTestPanel extends JPanel{
-	
-	
+
+
 	private JLabel question;
-	
+
 	private JButton optionA;
 	private JButton optionB;
 	private JButton optionC;
 	private JButton optionD;
-	
+
 	private int buildingKey;
-	
+
 	private ArrayList<String> questions = new ArrayList<String>();
-		
+	private ArrayList<String> answers = new ArrayList<String>();	
+	
 	private HashMap<Integer, String> questionsByKey= new HashMap<Integer, String>();
-	
-	
+	private HashMap<Integer,LinkedList<String>> answersByKey = new HashMap<Integer,LinkedList<String>>();
+
+
 	//GETTER
 	public int getBuildingKey() { return buildingKey; }
 
 	//SETTER
 	public void setBuildingKey(int buildingKey) { this.buildingKey = buildingKey; }
-	
-	
+
+
+
 	public TakeTestPanel(JFrame frame, int key) {			
-			
+
 		frame.getContentPane().add(this);
 		this.setVisible(true);	
 		this.setBuildingKey(key);
-		
+		this.setLayout(null);
+
 		getQuestion();
-		
+		getAnswers();
+
 		initialize();
-		
-		
 	}
-	
+
 	public void initialize() {				
-		
+
 		Font obj = new Font("Arial", Font.BOLD, 18);
-		setLayout(null);
-		
-		
-		//LABELS
-		JLabel label = new JLabel("1)");
-		label.setBounds(23, 63, 46, 14);
-		add(label);
-		
-		JLabel label_1 = new JLabel("2)");
-		label_1.setBounds(23, 107, 46, 14);
-		add(label_1);
-		
-		JLabel label_2 = new JLabel("3)");
-		label_2.setBounds(23, 154, 46, 14);
-		add(label_2);
-		
-		JLabel label_3 = new JLabel("4)");
-		label_3.setBounds(23, 198, 46, 14);
-		add(label_3);
-		
-		
+	
 		//QUESTION
 		question = new JLabel(questions.get(0));
 		question.setFont(obj);
 		question.setBounds(23, 16, 374, 22);
 		this.add(question);
+
+
+		//LABELS (ANSWERS)	
+		JLabel label_1 = new JLabel(answers.get(0));
+		label_1.setBounds(23, 63, 103, 14);
+		add(label_1);
+
+		JLabel label_2 = new JLabel(answers.get(1));
+		label_2.setBounds(23, 107, 103, 14);
+		add(label_2);
+
+		JLabel label_3 = new JLabel(answers.get(2));
+		label_3.setBounds(23, 154, 103, 14);
+		add(label_3);
+
+		JLabel label_4 = new JLabel(answers.get(3));
+		label_4.setBounds(23, 198, 103, 14);
+		add(label_4);
+
 		
-		
-		//BUTTONS (ANSWERS)		
-		optionA = new JButton("Select");
-		optionA.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
+		//OPTION 1
+		optionA = new JButton("Select");		
+		optionA.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
 				
 			}
 		});
-		optionA.setBounds(43, 59, 76, 23);
+		optionA.setBounds(152, 59, 80, 23);
 		this.add(optionA);
-		
+
+
+		//OPTION 2
 		optionB = new JButton("Select");
-		optionB.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				//this.remove(this);
-			}
-		});
-		optionB.setBounds(43, 194, 76, 23);
-		this.add(optionB);
-		
-		optionC = new JButton("Select");
-		optionC.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-
-			}
-		});
-		optionC.setBounds(43, 150, 76, 23);
-		this.add(optionC);
-		
-		optionD = new JButton("Select");
-		optionD.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-
-			}
-		});
-		optionD.setBounds(43, 103, 76, 23);
-		this.add(optionD);			
-		
-	}
-	
-	public void getQuestion() {
-		
-		questionsByKey = Reader.getQuestionList().get(getBuildingKey()-1);
-			
-		
-		for(HashMap.Entry<Integer, String> z : questionsByKey.entrySet()) {
-			
-				questions.add(z.getValue());			
+		optionB.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
 				
+				
+			}
+		});
+		optionB.setBounds(152, 103, 80, 23);
+		this.add(optionB);
+
+
+		//OPTION 3
+		optionC = new JButton("Select");
+		optionC.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				
+				
+			}
+		});
+		optionC.setBounds(152, 150, 80, 23);
+		this.add(optionC);
+
+
+		//OPTION 4
+		optionD = new JButton("Select");
+		optionD.addMouseListener(new MouseAdapter() {
+			@Override			
+			public void mouseClicked(MouseEvent arg0) {
+				
+				
+			}
+		});		
+		optionD.setBounds(152, 194, 80, 23);
+		this.add(optionD);
+
+
+	}
+
+	public void getQuestion() {
+
+		questionsByKey = Reader.getQuestionList().get(getBuildingKey()-1);			
+
+		for(HashMap.Entry<Integer, String> z : questionsByKey.entrySet()) {
+
+			questions.add(z.getValue());					
 		}
 	}
-	
+
+	public void getAnswers() {
+
+		//answers regarding a specific building
+		answersByKey = Reader.getQuestionAnswersList().get(getBuildingKey()-1);
+
+		for(HashMap.Entry<Integer,LinkedList<String>> buildingAnswers : answersByKey.entrySet()) {
+
+			for(String answ : buildingAnswers.getValue()) {
+
+				answers.add(answ);				
+			}
+		}		
+	}	
 }
